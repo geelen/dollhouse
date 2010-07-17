265aa2f5b89673abc6773910b9903684536f214c
Glen Maddern <glenmaddern@gmail.com>
2010-07-17 22:19:59 +1000

    sketching out command-line action

--- app/rackspace_cloud_adapter.rb
+++ app/rackspace_cloud_adapter.rb
@@ -13,7 +13,15 @@ class RackspaceCloudAdapter < CloudAdapter
 
   def boot_new_server(name, callback, opts)
     flavor_num = FLAVORS[opts[:instance_type]]
-    raise "Unknown instance_type of #{instance_type.inspect}. Permitted values are #{FLAVORS.keys.inspect}"
+    raise "Unknown instance_type of #{opts[:instance_type].inspect}. Permitted values are #{FLAVORS.keys.inspect}" unless flavor_num
+    image_num = IMAGES[opts[:os]]
+    raise "Unknown os of #{opts[:os].inspect}. Permitted values are #{IMAGES.keys.inspect}" unless image_num
 
+    server = @conn.servers.create(:flavor_id => flavor_num, :image_id => image_num, :name => name)
+
+    # make this asynch soon
+    server.wait_for { ready? }
+
+    callback.call
   end
 end
--- bin/dollhouse.rb
+++ bin/dollhouse.rb
@@ -3,9 +3,24 @@
 require 'rubygems'
 require 'main'
 
+PROJECT_ROOT = File.dirname(__FILE__) + "/../.."
+Dir.glob(PROJECT_ROOT + '/app/*.rb').each { |f| require f }
+
 Main do
   mode 'deploy' do
     argument('deployment') { required }
+    argument('prefix')
+    def run
+      Dollhouse.launch_from(Dir.pwd)
+      Dollhouse.initiate_deployment(params['deployment'].to_sym, :prefix => params['prefix'])
+    end
+  end
 
+  mode 'exec' do
+    argument('server_name') { required }
+    argument('cmd') { required }
+    def run
+
+    end
   end
 end
