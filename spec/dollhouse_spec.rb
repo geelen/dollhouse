c212cb9fda76ca1a6a57f940a5f810e67c186876
Glen Maddern <glenmaddern@gmail.com>
2010-07-17 21:24:07 +1000

    probably should start writing specs, but not yet

---
+++ spec/dollhouse_spec.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/support/env.rb'
+
+describe Dollhouse do
+  context "starting a single server" do
+    it "should work" do
+      #should write some specs
+      true.should be_true
+    end
+  end
+end
+
