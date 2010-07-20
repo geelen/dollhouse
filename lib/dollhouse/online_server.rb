e94d4bf7b0ae9fbedeb9137c952b2ab502cf964c
Glen Maddern <glenmaddern@gmail.com>
2010-07-20 13:16:36 +1000

    var uploading works!

--- lib/dollhouse/online_server.rb
+++ lib/dollhouse/online_server.rb
@@ -1,15 +1,14 @@
 module Dollhouse
   class OnlineServer < Struct.new(:name_in_cloud, :server_name, :status)
     def bootstrap
-      Dollhouse.cloud_adapter.execute(name_in_cloud, %Q{headless=true bash -c "`wget -O- babushka.me/up/hard,next`"})
-      Dollhouse.cloud_adapter.execute(name_in_cloud, %Q{babushka sources -a geelen git://github.com/geelen/babushka-deps})
+      Dollhouse.cloud_adapter.execute(name_in_cloud, %Q{headless=true bash -c "`wget -O- babushka.me/up/hard`"})
     end
 
     def babushka taskname, vars = {}
       if !vars.empty?
-        Dollhouse.cloud_adapter.write_file(name_in_cloud, "~/.babushka/vars/#{taskname}", {
-          :vars => vars.map_keys(&:to_s)
-        })
+        Dollhouse.cloud_adapter.write_file(name_in_cloud, ".babushka/vars/#{taskname}", {
+          :vars => vars.map_keys(&:to_s).map_values { |v| {:value => v}}
+        }.to_yaml)
         Dollhouse.cloud_adapter.execute(name_in_cloud, "babushka '#{taskname}' --defaults")
       else
         Dollhouse.cloud_adapter.execute(name_in_cloud, "babushka '#{taskname}'")
